b60a03d7433cdd234ff3f6a2aa92da19f224d55d
tarutaru09 <tarutaru09@users.noreply.github.com>
2021-12-21 09:13:49 +0000

    deploy: 9957058dff9cd9e0b301c1cadaf950635e9e2da0

---
+++ _next/static/xijVRwZmSQqUd-QssRGip/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-c98103972be1d7024d11.js","static/chunks/pages/index-8de95d9b6cda90091a0a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
